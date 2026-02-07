af91a6811959f7aeeb8cb20bd357750ca4d78400
deb0pam <debopamdas8@gmail.com>
2026-02-07 10:06:36 +0530

    implemented story pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
 import Navbar from './components/Navbar.jsx'
 import Features from './components/Features.jsx'
+import Story from './components/Story.jsx'
 const App=()=>{
   return(
     <main className="relative min-h-screen w-screen overflow-x-hidden">
@@ -9,6 +10,7 @@ const App=()=>{
       <About/>
       <Navbar/>
       <Features/>
+      <Story/>
     </main>
   )
 }
