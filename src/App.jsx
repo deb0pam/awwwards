32b0a81a33539f84c9c214ced91e6284764cddbd
deb0pam <debopamdas8@gmail.com>
2026-02-07 09:05:21 +0530

    implemented features pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
 import Navbar from './components/Navbar.jsx'
+import Features from './components/Features.jsx'
 const App=()=>{
   return(
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero/>
       <About/>
       <Navbar/>
+      <Features/>
     </main>
   )
 }
